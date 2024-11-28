34568ecea37e92e4cf7a43a8ec79bd7cb9bb119c
debisela <debora.jaeckel@gmail.com>
2024-11-28 20:30:56 +0200

    fix form

--- react-portfolio/src/components/Contact_form.jsx
+++ react-portfolio/src/components/Contact_form.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Resume_download from "./Resume_download";
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+      });
+
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Data Submitted:", formData);
+      };
+
   return (
     <>
       <section id="contact">
@@ -11,10 +30,25 @@ const Contact = () => {
         <div className="contact-container">
           {/* Contact Form */}
           <div className="contact-form-container">
-            <form action="#" method="POST" className="contact-form">
-              <input type="text" placeholder="Name" required />
-              <input type="email" placeholder="Email" required />
-              <textarea placeholder="Your message" required></textarea>
+            <form onSubmit={handleSubmit} className="contact-form">
+              <input 
+              type="text"
+              name="name"               
+              placeholder="Name" 
+              value={formData.name}
+              onChange={handleInputChange}
+              required />
+              <input type="email" 
+              name="email"
+              placeholder="Email" 
+              value={formData.email}
+              onChange={handleInputChange}
+              required />
+              <textarea 
+              name="message"
+              placeholder="Your message" value={formData.message}
+              onChange={handleInputChange}
+              required></textarea>
               <button type="submit">Send</button>
             </form>
           </div>
